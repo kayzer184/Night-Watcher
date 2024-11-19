7405d304d88dc54ebb3835a410b27312906a38ce
kayzer184 <157171606+kayzer184@users.noreply.github.com>
2024-11-19 14:39:25 +0300

    Update NPCLoader.js

--- src/Components/NPCLoader.js
+++ src/Components/NPCLoader.js
@@ -5,7 +5,8 @@ const NPCSpawns = [
   [80, 2, -80],
   [-80, 2, -80],
   [-680, 2, -80],
-  [80, 2, 1270],
+  [80, 2, 1280],
+  [-680, 2, 1280],
 ];
 
 const NPCPaths = [
@@ -48,27 +49,48 @@ const NPCPaths = [
   ],
   [
     { x: 80, z: 1280 },
-    { x: 80, z: -80 },
+    { x: 80, z: 1120 },
+    { x: -80, z: 1120 },
+    { x: -80, z: 520 },
+    { x: -80, z: 520 },
+    { x: -680, z: 520 },
+    { x: -680, z: 80 },
+    { x: 80, z: 80 },
     { x: 80, z: 680 },
+    { x: -680, z: 680 },
+    { x: -680, z: 1120 },
+    { x: -520, z: 1120 },
+    { x: -520, z: 1280 },
+    { x: 80, z: 1280 },
+  ],
+  [
+    { x: -680, z: 1280 },
+    { x: -680, z: 680 },
     { x: -80, z: 680 },
-  ]
+    { x: -80, z: 1120 },
+    { x: 80, z: 1120 },
+    { x: 80, z: 1280 },
+    { x: -680, z: 1280 },
+  ],
 ];
+
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 
 export default function NPCLoader(NPCObjects, NPCMaxCount, mixers, scene) {
   const NPCModelLoader = new FBXLoader();
 
-  // Создаем массив индексов для спавнов
   const indices = Array.from({ length: NPCSpawns.length }, (_, i) => i);
-
-  // Перемешиваем индексы для рандомного выбора
-  const shuffledIndices = indices.sort(() => Math.random() - 0.5);
-
-  // Ограничиваем количество NPC до `NPCMaxCount` или доступных спавнов
-  const spawnIndices = shuffledIndices.slice(0, Math.min(NPCMaxCount, NPCSpawns.length));
+  shuffle(indices);
+  const spawnIndices = indices.slice(0, Math.min(NPCMaxCount, NPCSpawns.length));
 
   spawnIndices.forEach((index) => {
     NPCModelLoader.load(
-      `/Models/NPC_${Math.round(Math.random() * 0)}.fbx`, // Укажите путь к модели
+      `/Models/NPC_${Math.round(Math.random() * 0)}.fbx`,
       (NPC) => {
         const mixer = new THREE.AnimationMixer(NPC);
         mixers.push(mixer);
@@ -76,7 +98,6 @@ export default function NPCLoader(NPCObjects, NPCMaxCount, mixers, scene) {
         const action = mixer.clipAction(NPC.animations[0], NPC);
         action.play();
 
-        // Берем точку спавна и соответствующий маршрут
         const spawn = NPCSpawns[index];
         const path = NPCPaths[index];
 
@@ -87,7 +108,7 @@ export default function NPCLoader(NPCObjects, NPCMaxCount, mixers, scene) {
         const npcData = {
           model: NPC,
           mixer: mixer,
-          path: path, // Назначаем соответствующий маршрут
+          path: path,
           currentTarget: 1,
           speed: 0.33,
           initialPosition: { x: spawn[0], y: spawn[1], z: spawn[2] },
