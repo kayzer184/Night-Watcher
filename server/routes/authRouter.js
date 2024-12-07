49d469e36d2480a29b748bdb020dd82d5d4b2f0b
kayzer <toer0257@gmail.com>
2024-12-07 18:54:59 +0300

    log fix #13

--- server/routes/authRouter.js
+++ server/routes/authRouter.js
@@ -13,7 +13,7 @@ router.post('/google', async (req, res) => {
     // Запрос данных пользователя с Google API
     const response = await fetch(`https://www.googleapis.com/oauth2/v3/userinfo?access_token=${accessToken}`);
     const data = await response.json();
-
+    console.log(data)
     res.status(200).json({
       message: `User authenticated successfully`,
       data: data,
