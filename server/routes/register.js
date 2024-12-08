221ad57d77a46a1ec52031cd3c6a123e35c74647
kayzer <toer0257@gmail.com>
2024-12-08 12:52:07 +0300

    server fix #2

--- server/routes/register.js
+++ server/routes/register.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
 
 router.post('/', async (req, res) => {
   try {
     const { googleId, username, email, createdAt } = req.body;
     const user = await User.create({ googleId, username, email, createdAt });
-    res.status(201).json({
+    res.status(200).json({
       message: 'User created successfully',
     });
   } catch (error) {
