2d91c1d305ab1b58ac781013c6d5cc3504569209
kayzer <toer0257@gmail.com>
2025-01-07 21:10:02 +0300

    Front update #5

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../Sass/App.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { GoogleOAuthProvider } from '@react-oauth/google'
@@ -8,21 +8,41 @@ import LeadersPage from '../Pages/LeadersPage'
 import SettingsPage from '../Pages/SettingsPage'
 import GamePage from '../Pages/GamePage'
 import NotFoundPage from '../Pages/NotFoundPage'
-import { AuthProvider } from '../context/AuthContext'
+import { AuthProvider, useAuth } from '../context/AuthContext'
+
+function AppRoutes() {
+	const { setUser } = useAuth()
+
+	useEffect(() => {
+		const handleStorageChange = () => {
+			const savedUser = localStorage.getItem('user')
+			if (!savedUser) {
+				setUser(null)
+			}
+		}
+
+		window.addEventListener('storage', handleStorageChange)
+		return () => window.removeEventListener('storage', handleStorageChange)
+	}, [setUser])
+
+	return (
+		<Routes>
+			<Route path='/' element={<MenuPage />} />
+			<Route path='/levels' element={<LevelsPage />} />
+			<Route path='/leaders' element={<LeadersPage />} />
+			<Route path='/settings' element={<SettingsPage />} />
+			<Route path='/game' element={<GamePage />} />
+			<Route path='*' element={<NotFoundPage />} />
+		</Routes>
+	)
+}
 
 function App() {
 	return (
 		<GoogleOAuthProvider clientId='208385145027-2rrf39t2f0f9fb0d03mrm2epjnlf1bgf.apps.googleusercontent.com'>
 			<AuthProvider>
 				<Router>
-					<Routes>
-						<Route path='/' element={<MenuPage />} />
-						<Route path='/levels' element={<LevelsPage />} />
-						<Route path='/leaders' element={<LeadersPage />} />
-						<Route path='/settings' element={<SettingsPage />} />
-						<Route path='/game' element={<GamePage />} />
-						<Route path='*' element={<NotFoundPage />} />
-					</Routes>
+					<AppRoutes />
 				</Router>
 			</AuthProvider>
 		</GoogleOAuthProvider>
