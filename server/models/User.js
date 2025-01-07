e903abbbd49c04bf28401ba9b6dc4336d6c98102
kayzer <toer0257@gmail.com>
2025-01-07 19:58:11 +0300

    Server update #10

--- server/models/User.js
+++ server/models/User.js
@@ -1,79 +1,49 @@
 const mongoose = require('mongoose')
 
 // Схема пользователя
-const userSchema = new mongoose.Schema({
-	googleId: {
-		type: String,
-		required: [true, 'Google ID is required'],
-		unique: true,
-		trim: true,
-		validate: {
-			validator: function (v) {
-				return v && v.length > 0
-			},
-			message: 'Google ID cannot be empty',
+const userSchema = new mongoose.Schema(
+	{
+		_id: {
+			type: mongoose.Schema.Types.ObjectId,
+			auto: true,
+		},
+		googleId: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			required: true,
+		},
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		achievements: {
+			type: Object,
+			default: {},
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+		},
+		__v: {
+			type: Number,
+			default: 0,
 		},
 	},
-	username: {
-		type: String,
-		required: [true, 'Username is required'],
-		trim: true,
-		minlength: [2, 'Username must be at least 2 characters long'],
-		validate: {
-			validator: function (v) {
-				return v && v.length >= 2
-			},
-			message: 'Invalid username format',
-		},
-	},
-	email: {
-		type: String,
-		required: [true, 'Email is required'],
-		unique: true,
-		trim: true,
-		lowercase: true,
-		validate: {
-			validator: function (v) {
-				return /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/.test(v)
-			},
-			message: props => `${props.value} is not a valid email address!`,
-		},
-	},
-	createdAt: {
-		type: Date,
-		default: Date.now,
-		required: true,
-	},
-	achievements: {
-		type: Object,
-		default: {},
-		required: true,
-	},
-})
+	{
+		collection: 'Users',
+		versionKey: '__v',
+	}
+)
 
-// Добавляем подробное логирование
-userSchema.pre('validate', function (next) {
-	console.log('Pre-validation data:', {
-		googleId: this.googleId,
-		username: this.username,
-		email: this.email,
-		hasAchievements: !!this.achievements,
-		createdAt: this.createdAt,
-	})
-	next()
-})
+// Добавляем индексы в соответствии с существующей структурой
+userSchema.index({ googleId: 1 }, { unique: true })
+userSchema.index({ email: 1 }, { unique: true })
 
-userSchema.pre('save', function (next) {
-	console.log('Pre-save data:', {
-		googleId: this.googleId,
-		username: this.username,
-		email: this.email,
-		hasAchievements: !!this.achievements,
-		createdAt: this.createdAt,
-	})
-	next()
-})
-
-const User = mongoose.model('User', userSchema, 'Users')
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
