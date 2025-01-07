49a13b69410e4d868ce63c1197d6a048664674dc
kayzer <toer0257@gmail.com>
2025-01-07 20:20:42 +0300

    Server update #16

--- server/models/User.js
+++ server/models/User.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-// Схема пользователя
 const userSchema = new mongoose.Schema({
 	googleId: {
 		type: String,
@@ -15,25 +14,15 @@ const userSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 		unique: true,
-		validate: {
-			validator: function (v) {
-				return /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/.test(v);
-			},
-			message: props => `${props.value} is not a valid email address!`,
-		},
-	},
-	achievements: {
-		type: Object,
-		default: {},
-		required: true
 	},
 	createdAt: {
 		type: Date,
 		default: Date.now,
 	},
-});
+	achievements: {
+		type: Object,
+		default: {},
+	},
+})
 
-// Убедимся, что модель не была определена ранее
-const User = mongoose.models.User || mongoose.model('Users', userSchema, 'Users');
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema, 'Users')
