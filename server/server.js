751db669cd0d3e35bb72098ade5ff456e1e9efa6
kayzer <toer0257@gmail.com>
2025-01-07 19:41:47 +0300

    Server update #2

--- server/server.js
+++ server/server.js
@@ -1,45 +1,58 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-const leaderboardRoutes = require('./routes/leaderboard');
-const registerRouter = require('./routes/register');
-const authRouter = require('./routes/authRouter');
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const leaderboardRoutes = require('./routes/leaderboard')
+const registerRouter = require('./routes/register')
+const authRouter = require('./routes/authRouter')
 
-const app = express();
+const app = express()
 
-app.use(cors({
-  origin: 'https://night-watcher.vercel.app', // Укажите ваш домен
-  methods: ['GET', 'POST', 'OPTIONS'], // Разрешенные методы
-  allowedHeaders: ['Content-Type', 'Authorization'], // Разрешенные заголовки
-}));
+app.use(
+	cors({
+		origin: ['https://night-watcher.vercel.app'],
+		methods: ['GET', 'POST', 'OPTIONS'],
+		allowedHeaders: ['Content-Type', 'Authorization'],
+		credentials: true,
+	})
+)
 
-app.use(express.json());
+app.use(express.json())
 
 // MongoDB URI
-const mongoURI = 'mongodb+srv://api_backend_user:bNm6rjubtsyEELwh@night-watcher.1nbdj.mongodb.net/Game';
+const mongoURI =
+	'mongodb+srv://api_backend_user:bNm6rjubtsyEELwh@night-watcher.1nbdj.mongodb.net/Game'
 
 // Подключение к MongoDB
-mongoose.connect(mongoURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
-  .then(() => {
-    console.log('MongoDB connected');
-  })
-  .catch((err) => {
-    console.error('Failed to connect to MongoDB:', err);
-    process.exit(1);
-  });
+mongoose
+	.connect(mongoURI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => {
+		console.log('MongoDB connected')
+	})
+	.catch(err => {
+		console.error('Failed to connect to MongoDB:', err)
+		process.exit(1)
+	})
+
+// Добавляем preflight для OPTIONS
+app.options('*', cors()) // Включаем preflight для всех маршрутов
 
 // Подключение маршрутов
-app.use('/getLeaderBoard', leaderboardRoutes);
-app.use('/register', registerRouter);
-app.use('/auth', authRouter);
+app.use('/getLeaderBoard', leaderboardRoutes)
+app.use('/register', registerRouter)
+app.use('/auth', authRouter)
 
 // Обработчик корневого маршрута
 app.get('/', (req, res) => {
-  res.send('Welcome to the Night-Watcher API');
-});
+	res.send('Welcome to the Night-Watcher API')
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`)
+})
 
 // Экспорт обработчика
-module.exports = app;
+module.exports = app
