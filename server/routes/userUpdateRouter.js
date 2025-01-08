ad2e191b0d64c5a5e4990ffd4038ed83fa1fb3b9
kayzer <toer0257@gmail.com>
2025-01-09 00:11:33 +0300

    server update #2

--- server/routes/userUpdateRouter.js
+++ server/routes/userUpdateRouter.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
-const LeaderboardRouter = require('./leaderboardRouter')
 
 router.post('/update', async (req, res) => {
     try {
