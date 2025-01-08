d54cfd8a1d930f22e7b5a85c87e10051d2a134d4
kayzer <toer0257@gmail.com>
2025-01-08 18:38:15 +0300

    Front update #20

--- client/src/Components/LevelStarsModal.js
+++ client/src/Components/LevelStarsModal.js
@@ -4,36 +4,29 @@ import { ReactComponent as StarIcon } from '../Assets/Icons/Star.svg'
 import '../Sass/LevelStarsModal.scss'
 
 const LevelStarsModal = ({ level, isPreview = false, gameStats = null }) => {
-	if (isPreview) {
-		// Упрощенное отображение для превью
-		return (
-			<div className='stars-preview'>
-				{[1, 2, 3].map(star => (
-					<StarIcon key={star} className='star-icon' />
+	return (
+		<div className='modal-stars-container'>
+			<div className='achievements-list'>
+				{Object.values(LEVELS_CONFIG[level].starConditions).map(condition => (
+					<div
+						key={condition.id}
+						className={`achievement-item ${
+							!isPreview && gameStats && condition.check(gameStats)
+								? 'completed'
+								: ''
+						}`}
+					>
+						<StarIcon
+							className={`achievement-star ${
+								!isPreview && gameStats && condition.check(gameStats)
+									? 'filled'
+									: ''
+							}`}
+						/>
+						<span className='achievement-text'>{condition.description}</span>
+					</div>
 				))}
 			</div>
-		)
-	}
-
-	// Полное отображение для игрового интерфейса
-	return (
-		<div className='stars-container'>
-			<h3>Достижения уровня</h3>
-			{Object.values(LEVELS_CONFIG[level].starConditions).map(condition => (
-				<div
-					key={condition.id}
-					className={`star-condition ${
-						gameStats && condition.check(gameStats) ? 'completed' : ''
-					}`}
-				>
-					<StarIcon
-						className={`star-icon ${
-							gameStats && condition.check(gameStats) ? 'filled' : ''
-						}`}
-					/>
-					<span className='condition-description'>{condition.description}</span>
-				</div>
-			))}
 		</div>
 	)
 }
