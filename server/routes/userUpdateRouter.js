27bd9d4167c91c4ca4e81942d3b92d1f3b3a874b
kayzer <toer0257@gmail.com>
2025-01-09 00:14:53 +0300

    server update #3

--- server/routes/userUpdateRouter.js
+++ server/routes/userUpdateRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const User = require('../models/User')
 
-router.post('/update', async (req, res) => {
+router.post('/', async (req, res) => {
     try {
         const { userId, levelId, achievements} = req.body
 
