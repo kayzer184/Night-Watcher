0f77b3d33c9b32415a913098ce5b61136226cf51
kayzer <toer0257@gmail.com>
2025-01-10 23:58:33 +0300

    server update

--- server/models/leaderboard.js
+++ server/models/leaderboard.js
@@ -4,7 +4,6 @@ const mongoose = require('mongoose')
 // Модель для таблицы лидеров
 const LeaderboardSchema = new mongoose.Schema({
 	username: { type: String, required: true },
-	score: { type: Number, required: true },
 	stars: { type: Number, required: true, default: 0 }, // Общее количество звезд
 	lastUpdated: { type: Date, default: Date.now }, // Для отслеживания последнего обновления
 })
