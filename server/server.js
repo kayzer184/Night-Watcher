1d2419d58674506483d0cb37411613d7c387c017
kayzer <toer0257@gmail.com>
2025-03-27 19:19:26 +0300

    feat(server): added server protection

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
-require('dotenv').config();
+require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
+const rateLimit = require('express-rate-limit')
+const helmet = require('helmet')
 const leaderboardRoutes = require('./routes/leaderboard')
 const authRouter = require('./routes/authRouter')
 const userExistRouter = require('./routes/userExist')
@@ -11,6 +13,10 @@ const userUpdateRouter = require('./routes/userUpdateRouter')
 
 const app = express()
 
+// Применяем защитные заголовки
+app.use(helmet())
+
+// CORS настройки
 app.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', 'https://night-watcher.vercel.app')
 	res.header('Access-Control-Allow-Credentials', 'true')
@@ -25,17 +31,46 @@ app.use((req, res, next) => {
 	next()
 })
 
-app.use(express.json())
+app.use(express.json({ limit: '96kb' }))
+
+const writeLimiter = rateLimit({
+	windowMs: 60 * 60 * 1000,
+	max: 50, 
+	standardHeaders: true,
+	message: 'Слишком много запросов на изменение данных'
+})
+
+const registerLimiter = rateLimit({
+	windowMs: 12* 60 * 60 * 1000,
+	max: 5,
+	standardHeaders: true,
+	message: 'Слишком много попыток регистрации'
+})
+
+const getLimiter = rateLimit({
+	windowMs: 15 * 60 * 1000,
+	max: 300,
+	standardHeaders: true,
+	message: 'Слишком много запросов на чтение данных',
+})
+
+app.use(getLimiter)
 
 // MongoDB URI
-const mongoURI = process.env.MONGO_URI;
+const mongoURI = process.env.MONGO_URI
 
-// Подключаем роуты до подключения к MongoDB
+// Подключаем роуты
 app.use('/auth', authRouter)
 app.use('/userExist', userExistRouter)
 app.use('/getLeaderBoard', leaderboardRoutes)
 app.use('/getUser', getUserRouter)
 app.use('/update', userUpdateRouter)
+
+app.use('/update', writeLimiter)
+app.use('/auth/*', registerLimiter)
+app.use('/getLeaderBoard', getLimiter)
+app.use('/getUser', getLimiter)
+app.use('/userExist', getLimiter)
 
 // Подключение к MongoDB
 mongoose
